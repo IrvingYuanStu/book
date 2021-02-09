44a88f49d4c36648453a2b5ebee323b9108b0b56
yuanyicheng <yuanyicheng@tuhu.cn>
2021-02-09 17:41:04 +0800

    add args[1]

--- src/main/java/com/java8/book/Test1.java
+++ src/main/java/com/java8/book/Test1.java
@@ -2,7 +2,7 @@ package com.java8.book;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world" + args[1]);
 
         System.out.println("bugfix分支，修改了一个bug");
     }
