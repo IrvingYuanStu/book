c6935c6e24ecef3674badae43ed3d8f731aaa700
yuanyicheng <yuanyicheng@tuhu.cn>
2021-02-10 14:23:21 +0800

    add test1

--- src/main/java/com/java8/book/Test1.java
+++ src/main/java/com/java8/book/Test1.java
@@ -5,5 +5,7 @@ public class Test1 {
         System.out.println("hello world, add lisi" + args[0]);
 
         System.out.println("bugfix分支，修改了一个bug");
+
+        System.out.println("test1111111");
     }
 }
